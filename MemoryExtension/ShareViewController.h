fed3f8d1a2d2d3ebd04f7e0c474ddb5956d0b0f8
isaacroldan <isaac.roldan@gmail.com>
2015-10-27 10:55:25 +0100

    added documentation to the ShareViewController methods

--- MemoryExtension/ShareViewController.h
+++ MemoryExtension/ShareViewController.h
@@ -13,11 +13,39 @@
 @class ShareViewCollectionViewDataSource;
 
 @interface ShareViewController : UIViewController
+
+/**
+ Show an alert in the view that will disappear in a few seconds.
+ */
 - (void)showMessage:(NSString *)message;
+
+/**
+ Set the current artist name. It will be displayed at the top of the view.
+ */
 - (void)updateArtistName:(NSString *)artistName;
+
+/**
+ Hide any alert still showing and reload the CollectionView
+ */
 - (void)reloadView;
+
+/**
+ Set the CollectionView dataSource and delegate
+ */
 - (void)setCollectionViewDataSource:(ShareViewCollectionViewDataSource *)dataSource;
+
+/**
+ Flip all cells to show the back of the card.
+ */
 - (void)resetAllCells;
+
+/**
+ Flip the cells at the given index to show the front of the card
+ */
 - (void)discoverCellsAtIndex:(NSInteger)firstIndex andIndex:(NSInteger)secondIndex;
+
+/**
+ Flip the cells at the given index to show the back of the card
+ */
 - (void)hideCellsAtIndex:(NSInteger)firstIndex andIndex:(NSInteger)secondIndex;
 @end
